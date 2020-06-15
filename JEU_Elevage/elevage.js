dcc7af55490f5828e57c37aa369f7412e196dc55
Carliplier <charles.remande@gmail.com>
2020-06-15 17:47:53 +0200

    Prod

---
+++ JEU_Elevage/elevage.js
@@ -0,0 +1,179 @@
+var config = {
+	type: Phaser.AUTO,
+	width: 600,
+	height: 800,
+physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: 0,
+            //debug: true
+        }
+    },
+scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var game = new Phaser.Game(config);	
+var player;
+var cursors; 
+
+
+
+function preload(){
+	
+	
+	this.load.image('fond','assets/fond_elevage.png')
+	this.load.image('bh','assets/barriere_h.png');
+	this.load.image('bv','assets/barriere_v2.png');
+	//this.load.spritesheet('animal','assets/animal.png',{frameWidth: 56, frameHeight: 41});
+	this.load.image('anim','assets/animal2.png');
+	this.load.image('buis','assets/buisson.png');
+}
+
+
+
+function create(){
+	var back = this.add.image(350,400,'fond');
+	
+	
+
+	
+	barriere = this.physics.add.staticGroup();
+	barriere.create(305,20,'bh');
+	barriere.create(305,560,'bh');
+	barriere.create(40,300,'bv');
+	barriere.create(570,300,'bv');
+	
+	
+	buisson = this.physics.add.staticGroup();
+	buisson.create(Phaser.Math.Between(60, 540),Phaser.Math.Between(60, 540),'buis');
+	buisson.create(Phaser.Math.Between(60, 540),Phaser.Math.Between(60, 540),'buis');
+	buisson.create(Phaser.Math.Between(60, 540),Phaser.Math.Between(60, 540),'buis');
+	buisson.create(Phaser.Math.Between(60, 540),Phaser.Math.Between(60, 540),'buis');
+	
+	
+	animal = this.physics.add.group();
+	animal = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal.setBounce(1);
+	animal.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal);
+	this.physics.add.collider(buisson,animal);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	animal2 = this.physics.add.group();
+	animal2 = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal2.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal2.setBounce(1);
+	animal2.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal2);
+	this.physics.add.collider(buisson,animal2);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	animal3 = this.physics.add.group();
+	animal3 = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal3.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal3.setBounce(1);
+	animal3.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal3);
+	this.physics.add.collider(buisson,animal3);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	animal4 = this.physics.add.group();
+	animal4 = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal4.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal4.setBounce(1);
+	animal4.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal4);
+	this.physics.add.collider(buisson,animal4);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	animal5 = this.physics.add.group();
+	animal5 = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal5.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal5.setBounce(1);
+	animal5.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal5);
+	this.physics.add.collider(buisson,animal5);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	animal6 = this.physics.add.group();
+	animal6 = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal6.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal6.setBounce(1);
+	animal6.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal6);
+	this.physics.add.collider(buisson,animal6);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+
+	animal7 = this.physics.add.group();
+	animal7 = this.physics.add.sprite(Phaser.Math.Between(70, 500),Phaser.Math.Between(70, 500),'anim');
+	animal7.setVelocity(Phaser.Math.Between(-150, 150),Phaser.Math.Between(-150, 150));
+	//animal.setVelocity(0,0);
+	animal7.setBounce(1);
+	animal7.setCollideWorldBounds(true);
+	this.physics.add.collider(barriere,animal7);
+	this.physics.add.collider(buisson,animal7);
+	this.anims.create({
+		
+		frames: this.anims.generateFrameNumbers('anim', {start: 0, end: 2}),
+		frameRate: 10,
+		repeat: -1
+	});
+
+
+	cursors = this.input.keyboard.createCursorKeys(); 
+	
+	
+
+}
+
+
+
+function update(){
+	
+	
+	
+}
+
+
+
---
+++ JEU_Peche/peche.js
@@ -0,0 +1,126 @@
+var config = {
+	type: Phaser.AUTO,
+	width: 600,
+	height: 800,
+physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: 0,
+            //debug: true
+        }
+    },
+scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var game = new Phaser.Game(config);	
+var player;
+var cursors; 
+
+
+
+function preload(){
+	
+	
+	this.load.image('fond','assets/fond_peche.png')
+	this.load.image('filet','assets/filet.png');
+	this.load.image('bateau','assets/bateau_peche.png');
+	this.load.image('ligne','assets/ligne_peche.png');
+	this.load.image('poisson','assets/poisson.png');
+	this.load.image('lignemer','assets/ligne_mer.png');
+}
+
+
+
+function create(){
+	var back = this.add.image(350,400,'fond');
+	var bateau = this.add.image(400,115,'bateau');
+	var ligne = this.add.image(235,520,'ligne');
+	
+	
+	lignemer = this.physics.add.staticGroup();
+	lignemer.create(300,225,'lignemer');
+	
+	
+	
+	
+	
+	
+	fish = this.physics.add.group();
+	fish = this.physics.add.sprite(Phaser.Math.Between(0,600),Phaser.Math.Between(230,800),'poisson');
+	fish.setVelocity(Phaser.Math.Between(-300, 300),Phaser.Math.Between(-50, 50));
+	fish.setCollideWorldBounds(true);
+	fish.setBounce(1);
+	this.physics.add.collider(lignemer,fish);
+	
+	
+	fish2 = this.physics.add.group();
+	fish2 = this.physics.add.sprite(Phaser.Math.Between(0,600),Phaser.Math.Between(230,800),'poisson');
+	fish2.setVelocity(Phaser.Math.Between(-300, 300),Phaser.Math.Between(-50, 50));
+	fish2.setCollideWorldBounds(true);
+	fish2.setBounce(1);
+	this.physics.add.collider(lignemer,fish2);
+	
+	
+	fish3 = this.physics.add.group();
+	fish3 = this.physics.add.sprite(Phaser.Math.Between(0,600),Phaser.Math.Between(230,800),'poisson');
+	fish3.setVelocity(Phaser.Math.Between(-300, 300),Phaser.Math.Between(-50, 50));
+	fish3.setCollideWorldBounds(true);
+	fish3.setBounce(1);
+	this.physics.add.collider(lignemer,fish3);
+	
+	
+	
+	player = this.physics.add.sprite(235,250,'filet');
+	player.setCollideWorldBounds(true);
+	this.physics.add.collider(player,lignemer);
+	
+	
+
+	cursors = this.input.keyboard.createCursorKeys(); 
+	
+	this.physics.add.overlap(player,fish, hitfish);	
+	this.physics.add.overlap(player,fish2, hitfish2);
+	this.physics.add.overlap(player,fish3, hitfish3);
+
+}
+
+
+
+function update(){
+	if(cursors.up.isDown){
+		player.setVelocityY(-200);
+	
+	}else if(cursors.down.isDown){
+		player.setVelocityY(200);
+		
+		
+	}else{
+		
+		player.setVelocityY(0);
+	}
+	
+	
+}
+
+function hitfish(player, fish){
+	
+
+	fish.disableBody(true,true);
+
+}
+function hitfish2(player, fish2){
+	
+
+	fish2.disableBody(true,true);
+
+}
+function hitfish3(player, fish3){
+	
+
+	fish3.disableBody(true,true);
+
+}
